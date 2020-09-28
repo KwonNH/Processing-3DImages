e28e2ebc77a207b60df05e9eeb3b7aafd0723d0d
kwon <skgus2624@gmail.com>
2020-09-27 20:16:22 -0500

    download images from urls

--- scraping_images.py
+++ scraping_images.py
@@ -1,43 +1,73 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotVisibleException
 import time
-import csv
 import pandas as pd
+import requests
+from requests.exceptions import SSLError
+from urllib3.exceptions import MaxRetryError
 
-driver = webdriver.Chrome('./chromedriver')
-driver.implicitly_wait(15)
 
-driver.get('https://www.google.com/search?q=3d+model+with+support+structure&tbm=isch&ictx=1&tbs=rimg:CZiV-IQeLpHSIgiYlfiEHi6R0ioSCZiV-IQeLpHSEVJC1lDYO5s4&hl=ko&sa=X&ved=2ahUKEwiA85XqnIrsAhUBVawKHRg5CcEQiRx6BAgAEAQ&biw=1366&bih=1024')
+def get_image_urls():
+    driver = webdriver.Chrome('./chromedriver')
+    driver.implicitly_wait(30)
 
-result = []
+    driver.get('https://www.google.com/search?q=3d+model+with+support+structure&tbm=isch&ictx=1&tbs=rimg:CZiV-IQeLpHSIgiYlfiEHi6R0ioSCZiV-IQeLpHSEVJC1lDYO5s4&hl=ko&sa=X&ved=2ahUKEwiA85XqnIrsAhUBVawKHRg5CcEQiRx6BAgAEAQ&biw=1366&bih=1024')
 
-for i in range(20, 200):
-    try:
-        thumbnail_image = driver.find_elements_by_class_name("rg_i")[i]
-        thumbnail_image.click()
-        image = driver.find_elements_by_css_selector(".n3VNCb")
+    result = []
 
-        for k in image:
-            src = k.get_attribute("src")
-            if "https" in src:
-                result.clear()
-                result.append([str(i), src])
-                print(result)
-        print("----")
+    time.sleep(15)
 
+    for i in range(100, 200):
         try:
-            result_df = pd.DataFrame(result, columns=['index', 'img_url'])
-            result_df.to_csv("./result.csv", mode='a', header=False, index=False)
-        except IndexError:
+            thumbnail_image = driver.find_elements_by_class_name("rg_i")[i]
+            thumbnail_image.click()
+            image = driver.find_elements_by_css_selector(".n3VNCb")
+
+            time.sleep(10)
+            result.clear()
+
+            for k in image:
+                src = k.get_attribute("src")
+                # avoid scraping thumbnail images below the target image
+                if "http" in src:
+                    if "jpg" in src or "jpeg" in src or "png" in src:
+                        result.append([str(i), src])
+                        print(result)
+            print("----")
+
+            try:
+                result_df = pd.DataFrame(result, columns=['index', 'img_url'])
+                result_df.to_csv("./result.csv", mode='a', header=False, index=False)
+            except IndexError:
+                pass
+
+            time.sleep(5)
+
+        except ElementNotVisibleException:
             pass
 
-        time.sleep(15)
-
-    except ElementNotVisibleException:
-        pass
+    driver.quit()
+    driver.close()
 
 
-driver.quit()
-driver.close()
+def download_images_from_urls():
 
+    urls = pd.read_csv("./result.csv")
+
+    for i in range(90, len(urls)):
+        print(urls.iloc[i])
+
+        try:
+            response = requests.get(urls.iloc[i][1])
+
+            file = open("./image_with_supports/" + str(urls.iloc[i][0]) + ".jpg", "wb")
+            file.write(response.content)
+            file.close()
+        except SSLError:
+            pass
+        except MaxRetryError:
+            pass
+
+
+if __name__ == "__main__":
+    download_images_from_urls()
